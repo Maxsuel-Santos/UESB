bf8d17c81aff5ed2af833b72657d347e01b183a9
Maxsuel Aparecido Lima Santos <72948249+Maxsuel-Santos@users.noreply.github.com>
2025-05-06 14:40:57 -0300

    ex02

---
+++ C++/Funções/Funções - Lista de Exercícios/ex02.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int quadrado(int num);
+
+int main() {
+    int num = 0;
+
+    cout << "Digite um valor: ";
+    cin >> num;
+
+    int resultado = quadrado(num);
+
+    cout << "O quadado de " << num << " e " << resultado;
+    return 0;
+}
+
+int quadrado(int num) {
+    return num * num;
+}
