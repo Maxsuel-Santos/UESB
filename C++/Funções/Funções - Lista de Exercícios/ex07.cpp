2495fe7be3d2bc40c313dc6e367a4ddf8c0947a1
Maxsuel Aparecido Lima Santos <72948249+Maxsuel-Santos@users.noreply.github.com>
2025-05-06 15:55:40 -0300

    ex07

---
+++ C++/Funções/Funções - Lista de Exercícios/ex07.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+unsigned int inverte(unsigned num);
+
+int main() {
+    unsigned int num = 0;
+
+    cout << "Digite um valor para ver ele invertido: ";
+    cin >> num;
+
+    cout << inverte(num);
+
+    return 0;
+}
+
+unsigned int inverte(unsigned num) {
+    int invertido = 0;
+
+    while(num != 0) {
+        unsigned int digito = num % 10;
+        invertido = invertido * 10 + digito;
+        num /= 10;
+    }
+
+    return invertido;
+}
