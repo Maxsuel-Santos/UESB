0b64ff657ba8de8401c4079a17059670fc97269a
Maxsuel Aparecido Lima Santos <72948249+Maxsuel-Santos@users.noreply.github.com>
2025-05-08 16:42:38 -0300

    ex10

--- C++/Funções/Funções - Lista de Exercícios/ex10.cpp
+++ C++/Funções/Funções - Lista de Exercícios/ex10.cpp
@@ -3,6 +3,7 @@
 using namespace std;
 
 int contaDigitos(int n, int d);
+int permutacao(int a, int b);
 
 int main() {
     int n = 0, d = 0;
@@ -13,7 +14,13 @@ int main() {
     cin >> d;
 
     int quant = contaDigitos(n, d);
-    cout << "O numero " << d << " apareceu " << quant << " vezes.";
+    cout << "O numero " << d << " apareceu " << quant << " vezes." << endl;
+
+    if(permutacao(n, d)) {
+        cout << "Sao permutacoes.";
+    } else {
+        cout << "Nao sao permutacoes.";
+    }
 
     return 0;
 }
@@ -29,9 +36,15 @@ int contaDigitos(int n, int d) {
             }        
             n = (int) n / 10;
         } 
-    } else {
-        return cont;
     }
-
     return cont;
+}
+
+int permutacao(int a, int b) {
+    for(int d = 1; d <= 9; d++) {
+        if(contaDigitos(a, d) != contaDigitos(b, d)) {
+            return false;
+        }
+    }
+    return true;
 }
