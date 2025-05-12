05f5dd6b86280f98bd9c3c76829852544fdc2f1f
Maxsuel Aparecido Lima Santos <72948249+Maxsuel-Santos@users.noreply.github.com>
2025-05-12 16:00:21 -0300

    ex39

---
+++ C++/Funções/Funções - Lista de Exercícios/ex39.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+template <typename T>
+void multiplos(T& soma, T x, int n) {
+    soma = 1;
+
+    for(int i = 1; i <= n; i++) {
+        soma += i * x;
+    }
+}
+
+int main() {
+    int x = 0, n = 0, soma = 0;
+
+    cout << "Valor de x: "; cin >> x;
+    cout << "Valor de n: "; cin >> n;
+
+    multiplos(soma, x, n); // soma foi passado por referência
+
+    cout << "Soma: " << soma;
+
+    return 0;
+}
