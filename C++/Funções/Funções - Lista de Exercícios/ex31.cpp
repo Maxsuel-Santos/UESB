f7af080309689f70be2ce6aba456e63db5d5b323
Maxsuel Aparecido Lima Santos <72948249+Maxsuel-Santos@users.noreply.github.com>
2025-05-21 10:42:26 -0300

    ex31 esqueleto do projeto

---
+++ C++/Funções/Funções - Lista de Exercícios/ex31.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+enum cartas {
+    as = 1, dois, tres, quatro, cinco, seis, sete, oito, nove, valete = 10, dama = 10, rei = 10
+};
+
+int compra_carta(int num);
+int recorde(int num);
+void jogo();
+
+int main() {
+    setlocale(LC_ALL, "Portuguese_Brazil");
+
+    return 0;
+}
+
+int compra_carta(int num) {
+
+}
+
+int recorde(int num) {
+
+}
+
+void jogo() {
+
+}
