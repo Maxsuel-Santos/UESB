95eafe05706f0fedbe31afac8bd81500d9815133
Maxsuel Aparecido Lima Santos <72948249+Maxsuel-Santos@users.noreply.github.com>
2025-05-21 10:49:37 -0300

    compra_carta

--- C++/Funções/Funções - Lista de Exercícios/ex31.cpp
+++ C++/Funções/Funções - Lista de Exercícios/ex31.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <ctime>
+#include <cstdlib>
 
 using namespace std;
 
@@ -6,7 +8,7 @@ enum cartas {
     as = 1, dois, tres, quatro, cinco, seis, sete, oito, nove, valete = 10, dama = 10, rei = 10
 };
 
-int compra_carta(int num);
+int compra_carta();
 int recorde(int num);
 void jogo();
 
@@ -16,8 +18,14 @@ int main() {
     return 0;
 }
 
-int compra_carta(int num) {
+int compra_carta() {
+    int carta = rand() % 13 + 1;
 
+    if(carta >= 11 && carta <= 13) {
+        return 10;
+    } else {
+        return carta;
+    }
 }
 
 int recorde(int num) {
