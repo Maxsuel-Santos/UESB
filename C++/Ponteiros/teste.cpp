c69715db675f5dd6adc91b63933a18c94692f22d
Maxsuel Aparecido Lima Santos <72948249+Maxsuel-Santos@users.noreply.github.com>
2025-06-25 20:36:06 -0300

    teste

--- C++/Ponteiros/teste.cpp
+++ C++/Ponteiros/teste.cpp
@@ -4,10 +4,15 @@ using namespace std;
 
 int main() {
     int var = 10;
+    int *ptr = &var; // ponteiros são passados por referência. O endereço de memória de 'var' está em ptr
 
-    cout << var; // valor de 'var'
+    cout << var; // valor de 'var' => 10
     cout << endl;
-    cout << &var; // endereco de 'var'
+    cout << &var; // endereco de 'var' => ex: 0x62ff08
+    cout << endl;
+    cout << *ptr; // exibe o valor da variável 'var' que foi passada por referência
+    cout << endl;
+    cout << ptr; // exibe o endereço de memória de 'var' que foi passada por referência
 
     return 0;
 }
