929667347e4bbf2b9dadfa8503829e8fc08d1659
Maxsuel Aparecido Lima Santos <72948249+Maxsuel-Santos@users.noreply.github.com>
2025-06-25 20:30:19 -0300

    teste

---
+++ C++/Ponteiros/teste.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int var = 10;
+
+    cout << var; // valor de 'var'
+    cout << endl;
+    cout << &var; // endereco de 'var'
+
+    return 0;
+}
