4a3666b60cdd932314eca6b4550ee32313109b36
Maxsuel Aparecido Lima Santos <72948249+Maxsuel-Santos@users.noreply.github.com>
2025-06-25 20:40:06 -0300

    teste

--- C++/Ponteiros/teste.cpp
+++ C++/Ponteiros/teste.cpp
@@ -6,13 +6,16 @@ int main() {
     int var = 10;
     int *ptr = &var; // ponteiros são passados por referência. O endereço de memória de 'var' está em ptr
 
-    cout << var; // valor de 'var' => 10
+    cout << "Conteudo de var: " << var; // valor de 'var' => 10
     cout << endl;
-    cout << &var; // endereco de 'var' => ex: 0x62ff08
+    cout << "Endereco de var: " <<  &var; // endereco de 'var' => ex: 0x62ff08
     cout << endl;
-    cout << *ptr; // exibe o valor da variável 'var' que foi passada por referência
+    cout << "Conteudo apontado por ptr: " << *ptr; // exibe o valor da variável 'var' que foi passada por referência
     cout << endl;
-    cout << ptr; // exibe o endereço de memória de 'var' que foi passada por referência
+    cout << "Endereco apontado por ptr: " << ptr; // exibe o endereço de memória de 'var' que foi passada por referência
 
+    // OBS: Conteúdo é diferente de endereço: 
+    // CONTEÚDO: É o valor, seja ele numérico, booleano ou alfabético, que está em uma variável;
+    // ENDEREÇO: É um 'código' que nos diz o local em que o conteúdo da variável está armazenada na memória ram.
     return 0;
 }
